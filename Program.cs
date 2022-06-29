9f916d6105501f2f8532a283af8c0fe1b5ebfee1
RuDlv <Defcon6@DESKTOP>
2022-06-29 20:48:20 +0300

    Added Swagger UI

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 global using WebApiApp.Data;
 global using Microsoft.EntityFrameworkCore;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -17,11 +18,26 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+//app.UseSwaggerUI(options =>
+//{
+//    //options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+//    options.RoutePrefix = string.Empty;
+
+
+//});
 
 app.UseHttpsRedirection();
 
